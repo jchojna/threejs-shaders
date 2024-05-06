c9d5d64b926223630b19d9285e14a3184d0483aa
Jakub Chojna <jakub.chojna@gmail.com>
2024-05-06 23:34:44 +0200

    chore: add simple three js scene

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import * as THREE from 'three';
+
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Hello, World!</h1>
-    <p>It's working!</p>
-  </div>
+  <canvas id="canvas"></canvas>
 `;
+
+const aspectRatio = 1;
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas');
+if (!canvas) throw new Error('Canvas not found');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(55, aspectRatio, 0.1, 1000);
+camera.position.z = 100;
+camera.position.y = 100;
+
+const geometry = new THREE.RawShaderMaterial({});
+
+// scene.add(camera, geometry);
+
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
